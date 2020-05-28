c12f32a3db37a38bfa74007ff831a04f19c4c3fe
Sayed Sadat <s.abdullahs1@gmail.com>
2020-05-28 10:46:30 -0700

    solve 3 bus

--- slp.jl
+++ slp.jl
@@ -3,5 +3,8 @@ Pkg.add("JuMP")
 Pkg.add("Ipopt")
 Pkg.add("PowerModels")
 println("Packages were successfully installed")
-import JuMP, Ipopt
+import JuMP, Ipopt, PowerModels
 println("Packages were successfully imported")
+
+run_ac_opf("matpower/case3.m", with_optimizer(Ipopt.Optimizer))
+
