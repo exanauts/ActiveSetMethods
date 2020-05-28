5af9be5fd6e1de19b7abdaff1d9da89802eaf436
Sayed Sadat <s.abdullahs1@gmail.com>
2020-05-28 11:42:18 -0700

    MATPOWER cases added to the directory

--- slp.jl
+++ slp.jl
@@ -1,10 +1,17 @@
-using Pkg
-Pkg.add("JuMP")
-Pkg.add("Ipopt")
-Pkg.add("PowerModels")
-println("Packages were successfully installed")
+#using Pkg
+#Pkg.add("JuMP")
+#Pkg.add("Ipopt")
+#Pkg.add("PowerModels")
+#println("Packages were successfully installed")
 import JuMP, Ipopt, PowerModels
 println("Packages were successfully imported")
 
-run_ac_opf("matpower/case3.m", with_optimizer(Ipopt.Optimizer))
+run_ac_opf("case3.m", with_optimizer(Ipopt.Optimizer))
 
+#network_data = PowerModels.parse_file("case3.m")
+
+#pm = instantiate_model(network_data, ACPPowerModel, PowerModels.build_opf)
+
+#print(pm.model)
+
+#result = optimize_model!(pm, optimizer=with_optimizer(Ipopt.Optimizer))
