a74b738a98cd0f32be78844e78716c353fa770df
Sayed Sadat <s.abdullahs1@gmail.com>
2020-05-29 12:03:48 -0700

    SLP function called

--- slp.jl
+++ slp.jl
@@ -9,16 +9,17 @@ eps=0.0001; itr_max=2
 
 
 #SLP function
-function slp(network_data)
+function slp(network)
 	#Initialization
 	# TODO: Add some initialization
 	itr=0; err=1;
+	bus_size=length(network["bus"])
 	while(err>eps && itr<itr_max) itr+=1;
 		lp=Model(with_optimizer(Ipopt.Optimizer, OutputFlag=0));
-		@variable(lp, x[1:2]>=0)
-		@variable(lp, y[1:2]>=0)
+		@variable(lp, x[1:bus_size]>=0)
+		@variable(lp, y[1:bus_size]>=0)
 
-		@constraint(lp, [i=1:2], x[i]+y[j] >= i*10);
+		@constraint(lp, [i=1:bus_size], x[i]+y[j] >= i*10);
 		
 		@objective(lp, Min, cx*sum(x)+cy*sum(y));
 		@time JuMP.optimize!(lp);
@@ -29,8 +30,11 @@ end
 
 
 #Read Network Data
-network_data = PowerModels.parse_file("cases/case3.m")
-println("Network Data Read Successfully")
+network = PowerModels.parse_file("cases/case3.m");
+println("Network Data Read Successfully");
+
+slp(network);
+println("SLP Optimization wa Successfully");
 
 
 
