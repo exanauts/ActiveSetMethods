3f2eb58ff698fbda8943d294930ee6e180fe23f5
Sayed Sadat <s.abdullahs1@gmail.com>
2020-05-29 11:25:48 -0700

    moved cases to cases dir

--- slp.jl
+++ slp.jl
@@ -1,17 +1,39 @@
-#using Pkg
-#Pkg.add("JuMP")
-#Pkg.add("Ipopt")
-#Pkg.add("PowerModels")
-#println("Packages were successfully installed")
+#Importing Packages
 using JuMP, Ipopt, PowerModels
 println("Packages were successfully imported")
 
-#run_ac_opf("case3.m", with_optimizer(Ipopt.Optimizer))
 
-network_data = PowerModels.parse_file("case3.m")
+#Defining Parameters
+eps=0.0001; itr_max=2
 
-pm = instantiate_model(network_data, ACPPowerModel, PowerModels.build_opf)
 
-print(pm.model)
 
-result = optimize_model!(pm, optimizer=with_optimizer(Ipopt.Optimizer))
+#SLP function
+function slp(network_data)
+	#Initialization
+	# TODO: Add some initialization
+	itr=0; err=1;
+	while(err>eps && itr<itr_max) itr+=1;
+		lp=Model(with_optimizer(Ipopt.Optimizer, OutputFlag=0));
+		@variable(lp, x[1:2]>=0)
+		@variable(lp, y[1:2]>=0)
+
+		@constraint(lp, [i=1:2], x[i]+y[j] >= i*10);
+		
+		@objective(lp, Min, cx*sum(x)+cy*sum(y));
+		@time JuMP.optimize!(lp);
+	  	println("Objective Value: ", JuMP.objective_value(lp), "(",JuMP.termination_status(lp),")");
+	end
+end
+
+
+
+#Read Network Data
+network_data = PowerModels.parse_file("cases/case3.m")
+println("Network Data Read Successfully")
+
+
+
+
+
+
