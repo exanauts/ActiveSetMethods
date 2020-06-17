e290d48dedae00443ee3a21b026f868e4e5795cc
Sayed Sadat <s.abdullahs1@gmail.com>
2020-06-17 08:21:56 -0700

    Update Slopt.jl

--- src/Slopt.jl
+++ src/Slopt.jl
@@ -1,8 +1,34 @@
 module Slopt
-export print_slopt
+export slopt_status
 
-function print_slopt()
+function slopt_status()
 println("Slopt module has been loaded successfully ...")
 end
+
+    using MathOptInterface
+    using TimerOutputs
+    using Arpack
+    using Printf
+    using SparseArrays
+    using LinearAlgebra
+
+    import Random
+    import LinearAlgebra: BlasInt
+
+    include("structs.jl")
+    #include("util.jl")
+    #include("printing.jl")
+    #include("scaling.jl")
+    #include("equilibration.jl")
+    #include("pdhg.jl")
+    #include("residuals.jl")
+    #include("eigsolver.jl")
+    #include("prox_operators.jl")
+
+    include("MOI_wrapper.jl")
+
+    MOIU.@model _ProxSDPModelData () (MOI.EqualTo, MOI.GreaterThan, MOI.LessThan) (MOI.Zeros, MOI.Nonnegatives, MOI.Nonpositives, MOI.PositiveSemidefiniteConeTriangle) () (MOI.SingleVariable,) (MOI.ScalarAffineFunction,) (MOI.VectorOfVariables,) (MOI.VectorAffineFunction,)
+
+    Solver(;args...) = MOIU.CachingOptimizer(_ProxSDPModelData{Float64}(), ProxSDP.Optimizer(args))
 
 end
