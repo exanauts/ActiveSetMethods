4720f0e5c225370a0208bb33a7b4d4af98bbe0ad
Sayed Sadat <s.abdullahs1@gmail.com>
2020-06-18 15:24:29 -0700

    Update ProxSDP.jl

--- src_template/ProxSDP.jl
+++ src_template/ProxSDP.jl
@@ -21,9 +21,9 @@ module ProxSDP
     include("prox_operators.jl")
 
     include("MOI_wrapper.jl")
-
+    println("##########-------> After all includes and before MOIU");
     MOIU.@model _ProxSDPModelData () (MOI.EqualTo, MOI.GreaterThan, MOI.LessThan) (MOI.Zeros, MOI.Nonnegatives, MOI.Nonpositives, MOI.PositiveSemidefiniteConeTriangle) () (MOI.SingleVariable,) (MOI.ScalarAffineFunction,) (MOI.VectorOfVariables,) (MOI.VectorAffineFunction,)
-
+    println("##########-------> After MOIU and before Solver");
     Solver(;args...) = MOIU.CachingOptimizer(_ProxSDPModelData{Float64}(), ProxSDP.Optimizer(args))
-
+    println("##########-------> After Solver");
 end
