5fa7ba45f2405f698be4cf8bf60bcef0cfd48319
Sayed Sadat <s.abdullahs1@gmail.com>
2020-07-02 13:52:01 -0600

    Update GLPK_example.jl

--- src/GLPK_example.jl
+++ src/GLPK_example.jl
@@ -34,7 +34,7 @@ MOI.set(model,
 MOI.set(model, MOI.ObjectiveSense(), MOI.MIN_SENSE);
 
 terms1 = MOI.VectorAffineTerm.(1:m1, MOI.ScalarAffineTerm.(A1, reshape(x, 1, n)));
-f1 = MOI.VectorAffineFunction(vec(terms1), b);
+f1 = MOI.VectorAffineFunction(vec(terms1), b1);
 MOI.add_constraint(model, f1, MOI.Nonpositives(m1));
 
 print(model);
