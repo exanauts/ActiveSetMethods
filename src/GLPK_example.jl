4baf9d7dc9b2fbe9a9b16102b18843a8238b94e6
Sayed Sadat <s.abdullahs1@gmail.com>
2020-07-02 13:42:44 -0600

    Create GLPK_example.jl

---
+++ src/GLPK_example.jl
@@ -0,0 +1,45 @@
+'''
+The model solves the following minimization problem.
+    
+      min   cx
+      s.b.  A1x + b1 = 0  
+            A2x + b2 >= 0
+
+x  -> n x 1
+c -> 1 x n
+A1 -> m1 x n
+b1 -> m1 x 1
+A2 -> m2 x n
+b1 -> m2 x 1
+'''
+
+using MathOptInterface, GLPK
+const MOI = MathOptInterface
+model = GLPK.Optimizer();
+
+n=4;
+m1=3;
+m2=2;
+
+c = rand(1,n);
+A1 = rand(m1,n);
+b1 = rand(m1,1);
+A2 = rand(m2,n);
+b2 = rand(m2,1);
+
+x = MOI.add_variables(model, n);
+MOI.set(model,
+        MOI.ObjectiveFunction{MOI.ScalarAffineFunction{Float64}}(),
+        MOI.ScalarAffineFunction(MOI.ScalarAffineTerm.(c, x), 0.0));
+MOI.set(model, MOI.ObjectiveSense(), MOI.MIN_SENSE);
+terms1 = MOI.VectorAffineTerm.(1:2, MOI.ScalarAffineTerm.(A1, reshape(x, 1, 3)));
+f1 = MOI.VectorAffineFunction(vec(terms), b);
+MOI.add_constraint(model, f1, MOI.Nonpositives(2));
+
+print(model);
+MOI.optimize!(model);
+
+
+
+
+
