ddca7f07c650b59f512639d5ef93d7c4fa37aa9d
Kibaek Kim <kibaek.k@gmail.com>
2020-08-20 22:21:25 -0500

    startomg unit test

--- test/unittests.jl
+++ test/unittests.jl
@@ -1,1 +1,15 @@
 using Test
+using ActiveSetMethods
+using JuMP
+
+model = Model(ActiveSetMethods.Optimizer);
+
+@variable(model, X);
+@variable(model, Y);
+@objective(model, Min, X^2 + X);
+@NLconstraint(model, X^2 - X == 2);
+@NLconstraint(model, X*Y == 1);
+@NLconstraint(model, X*Y >= 0);
+@constraint(model, X >= -2);
+
+asm = backend(model).optimizer.model
