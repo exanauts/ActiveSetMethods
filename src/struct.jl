d0052e513685669cbfa6bcab764bf7d4d7941da9
Sayed Sadat <s.abdullahs1@gmail.com>
2020-08-25 21:00:38 -0600

    Comments added and Nlopt changed to Nopt

--- src/struct.jl
+++ src/struct.jl
@@ -1,8 +1,9 @@
-mutable struct NloptProblem
-    #ef::Ptr{Cvoid}  # Reference to the internal data structure
-    n::Int  # Num vars
-    m::Int  # Num cons
+mutable struct NoptProblem
+    n::Int  # Number of variables
+    m::Int  # Number of constraints
+    #TODO chenage x to var
     x::Vector{Float64}  # Starting and final solution
+    #TODO chenage x_L to var_lb, x_U to var_ub, g to cons, g_L to cons_lb, g_U to cons_ub and so on
     x_L::Vector{Float64} # Variables Lower Bound
     x_U::Vector{Float64} # Variables Upper Bound
     g::Vector{Float64}  # Final constraint values
@@ -17,36 +18,37 @@ mutable struct NloptProblem
     status::Int  # Final status
 
     # Callbacks
-    eval_f::Function
-    eval_g::Function
-    eval_grad_f::Function
-    eval_jac_g::Function
-    eval_norm_E
-    eval_merit
-    eval_D
+    #TODO add their arguments in the comments
+    eval_f::Function # Function to evaluate objective function
+    eval_g::Function # Function to evaluate constraints
+    eval_grad_f::Function # Function to evaluate gradiant of objective function
+    eval_jac_g::Function # Function to evaluate jacobian (gradiant) of constraints
+    eval_norm_E::Function # Function to calculate the norm of constraint violations
+    eval_merit::Function # Function to evaluate the merit function
+    eval_D::Function # Function to evaluate directional derivative of merit function
 
-    eval_h  # Can be nothing
+    eval_h  # Can be nothing -- Function to evaluate the hessian H
     intermediate  # Can be nothing
 
     # For MathProgBase
-    sense::Symbol
-
-    function NloptProblem(n, x_L, x_U, m, g_L, g_U, j_sparsity, h_sparsity,
+    sense::Symbol 
+    
+    # Function to create new objective for the NoptProblem struct class
+    function NoptProblem(n, x_L, x_U, m, g_L, g_U, j_sparsity, h_sparsity,
                           eval_f, eval_g, eval_grad_f, eval_jac_g,eval_norm_E,
                           eval_merit, eval_D, eval_h)
-        prob = new(n, m, zeros(Float64, n), x_L, x_U, zeros(Float64, m), g_L, g_U,
+        #Initializes the object with given or default values
+        problem = new(n, m, zeros(Float64, n), x_L, x_U, zeros(Float64, m), g_L, g_U,
                    j_sparsity, h_sparsity, zeros(Float64,m), zeros(Float64,n),
                    zeros(Float64,n), 0.0, 0, eval_f, eval_g, eval_grad_f,
                    eval_jac_g, eval_norm_E, eval_merit, eval_D, eval_h, nothing,
                    :Min)
-        # Free the internal IpoptProblem structure when
-        # the Julia IpoptProblem instance goes out of scope
-        # finalizer(freeProblem, prob)
-        return prob
+        return problem
     end
 end
 
-# From Ipopt/src/Interfaces/IpReturnCodes_inc.h
+# Application return status copied from Ipopt.jl
+#TODO each of it needs to cross checked and verified
 ApplicationReturnStatus = Dict(
 0=>:Solve_Succeeded,
 1=>:Solved_To_Acceptable_Level,
