ffcd4780096c8d4541c03fa0db58474baf91122d
Kibaek Kim <kibaek.k@gmail.com>
2020-09-03 20:41:07 -0500

    Update opf.jl

--- examples/acopf/opf.jl
+++ examples/acopf/opf.jl
@@ -8,9 +8,6 @@ function run_opf(data_file::String)
     
     return optimize_model!(pm, optimizer = optimizer_with_attributes(
         ActiveSetMethods.Optimizer, 
-        "external_optimizer" => GLPK.Optimizer(),
-        "max_iter" => 20,
-        "eta" => 0.1,
-        "min_alpha" => 1.e-4,
-        "mu" => 1.e+4))
+        "external_optimizer" => GLPK.Optimizer()
+    ))
 end
