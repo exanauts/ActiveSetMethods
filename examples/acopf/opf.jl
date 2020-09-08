10c6f8c51ec35862af053fc63be259a49965f85f
Kibaek Kim <kibaek.k@gmail.com>
2020-09-08 13:50:07 -0500

    added arguments for opf example

--- examples/acopf/opf.jl
+++ examples/acopf/opf.jl
@@ -1,11 +1,11 @@
 using ActiveSetMethods
 using PowerModels, JuMP, GLPK
 
-function run_opf(data_file::String)
+function run_opf(data_file::String, 
+                 model = ACRPowerModel,
+                 build_function = PowerModels.build_opf)
     network_data = PowerModels.parse_file(data_file)
-    
-    pm = instantiate_model(network_data, ACRPowerModel, PowerModels.build_opf)
-    
+    pm = instantiate_model(network_data, model, build_function)
     return optimize_model!(pm, optimizer = optimizer_with_attributes(
         ActiveSetMethods.Optimizer, 
         "external_optimizer" => GLPK.Optimizer()
