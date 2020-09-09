ba585a92c403ccb3ca6e02b75560c9be98a8be9c
Kibaek Kim <kibaek.k@gmail.com>
2020-09-09 10:58:18 -0500

    added argument max_iter

--- examples/acopf/opf.jl
+++ examples/acopf/opf.jl
@@ -6,9 +6,10 @@ build_acr(data_file::String) = instantiate_model(PowerModels.parse_file(data_fil
 build_iv(data_file::String) = instantiate_model(PowerModels.parse_file(data_file), IVRPowerModel, PowerModels.build_opf_iv)
 
 run_opf(data_file::String) = run_opf(build_acp(data_file))
-run_opf(pm::AbstractPowerModel) = optimize_model!(pm, optimizer = optimizer_with_attributes(
+run_opf(pm::AbstractPowerModel, max_iter::Int = 100) = optimize_model!(pm, optimizer = optimizer_with_attributes(
     ActiveSetMethods.Optimizer, 
-    "external_optimizer" => GLPK.Optimizer()
+    "external_optimizer" => GLPK.Optimizer(),
+    "max_iter" => max_iter
 ))
 
 include("init_opf.jl")
@@ -21,6 +22,8 @@ init_vars(pm)
 # or 
 init_vars_from_ipopt(pm, build_acp("case3.m"))
 run_opf(pm)
+
+pm = build_acr("case3.m")
 
 pm = build_iv("case3.m")
 
